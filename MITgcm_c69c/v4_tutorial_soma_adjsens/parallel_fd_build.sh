2d0d74b9b458c32b9bbcaca09f7f85863ef21a80
Tanvir Shahriar <tshahriar@sverdrup.oden.utexas.edu>
2025-07-23 18:16:43 -0500

    update

---
+++ MITgcm_c69c/v4_tutorial_soma_adjsens/parallel_fd_build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Set root directory for MITgcm (absolute path is safest)
+MITGCM_ROOT=/home/tshahriar/Proj_ImPACTS/MITgcm_c69c/MITgcm
+
+# Go to build directory
+cd build_fd_parallel || { echo "Failed to enter build_fd_parallel"; exit 1; }
+
+# Clean any previous build
+make CLEAN
+
+# Configure the build
+$MITGCM_ROOT/tools/genmake2 -mpi \
+    -of=$MPI_OPTFILE \
+    -rd=$MITGCM_ROOT \
+    -mods=../code
+
+# Generate dependency list
+make depend
+
+# Build the forward model using 8 threads
+make -j 8
