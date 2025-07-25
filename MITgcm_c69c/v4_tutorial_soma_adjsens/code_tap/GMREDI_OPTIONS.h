f13f2cb730c34b729bbdcadbecf4e9ce25fa26e8
Tanvir Shahriar <tshahriar@sverdrup.oden.utexas.edu>
2025-07-25 14:54:00 -0500

    update

--- MITgcm_c69c/v4_tutorial_soma_adjsens/code_tap/GMREDI_OPTIONS.h
+++ MITgcm_c69c/v4_tutorial_soma_adjsens/code_tap/GMREDI_OPTIONS.h
@@ -19,7 +19,7 @@ CEOP
 C     Package-specific Options & Macros go here
 
 C Designed to simplify the Ajoint code:
-#define GMREDI_WITH_STABLE_ADJOINT
+C #define GMREDI_WITH_STABLE_ADJOINT
 C -- exclude the clipping/tapering part of the code that is not used
 #define GM_EXCLUDE_CLIPPING
 #define GM_EXCLUDE_FM07_TAP
@@ -29,8 +29,8 @@ C #define GM_EXCLUDE_TAPERING
 
 C Allows to read-in background 3-D Redi and GM diffusivity coefficients
 C Note: need these to be defined for use as control (pkg/ctrl) parameters
-#define GM_READ_K3D_REDI
-#define GM_READ_K3D_GM
+#undef GM_READ_K3D_REDI
+#undef GM_READ_K3D_GM
 
 C This allows to use Visbeck et al formulation to compute K_GM+Redi
 #undef GM_VISBECK_VARIABLE_K
